85ca247ccee3a927f8e858452d67c51f54dd3fa6
DongHyeok Lim <jh07050@gmail.com>
2024-10-24 15:22:57 +0900

    Remove: TEAM Feature 제거

--- src/main/java/woozlabs/echo/domain/team/repository/TeamSharedEmailRepository.java
+++
@@ -1,7 +0,0 @@
-package woozlabs.echo.domain.team.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import woozlabs.echo.domain.team.entity.TeamSharedEmail;
-
-public interface TeamSharedEmailRepository extends JpaRepository<TeamSharedEmail, Long> {
-}
--- src/main/java/woozlabs/echo/domain/team/repository/UserPublicKeyRepository.java
+++
@@ -1,9 +0,0 @@
-package woozlabs.echo.domain.team.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import woozlabs.echo.domain.team.entity.UserPublicKey;
-
-public interface UserPublicKeyRepository extends MongoRepository<UserPublicKey, String> {
-
-    UserPublicKey findByUid(String uid);
-}
